6ca257250bf197fdcdd328563d7a6b576b7733e9
Christopher Brinker <christopherbrinker@web.de>
2022-10-25 15:45:25 +0200

    introduced initialization promise for async database methods

--- src/util/PostGisConnection.ts
+++ src/util/PostGisConnection.ts
@@ -1,5 +1,4 @@
 import { createReadStream } from "fs";
-import "reflect-metadata";
 import { Features } from "src/entities/features.js";
 import { Collections } from "src/entities/collections.js";
 import { Jobs, JobState } from "src/entities/jobs.js";
@@ -10,11 +9,12 @@ import pgcopy from "pg-copy-streams";
 import { pipeline } from "stream/promises";
 
 export class PostGisConnection {
-  conn: DataSource;
+  conn: DataSource; // = null <--
+  connPromise: Promise<DataSource>;
 
-  async connectDB() {
+  async connectDB(): Promise<DataSource> {
     try {
-      const dataSource = new DataSource({
+      this.conn = new DataSource({
         type: "postgres",
         host: "localhost",
         port: Number(process.env.PG_PORT),
@@ -25,13 +25,18 @@ export class PostGisConnection {
         synchronize: true,
       });
 
-      this.conn = await dataSource.initialize();
+      return this.conn.initialize();
     } catch (err) {
-      console.log("Error while trying to connect to database:\n" + err);
+      throw new Error("Error while trying to connect to database:\n" + err);
     }
   }
+
   constructor() {
-    this.connectDB();
+    this.connPromise = this.connectDB();
+  }
+
+  async initialized() {
+    await this.connPromise;
   }
 
   /**
@@ -39,6 +44,8 @@ export class PostGisConnection {
    * @returns id of the freshly generated collection
    */
   async createNewCollection(): Promise<string> {
+    await this.initialized();
+
     const newColl = Collections.create({});
     await newColl.save();
 
@@ -50,6 +57,8 @@ export class PostGisConnection {
    * @returns
    */
   async createNewJob(): Promise<string> {
+    await this.initialized();
+
     const newJob = Jobs.create({});
     await newJob.save();
 
@@ -61,6 +70,8 @@ export class PostGisConnection {
   }
 
   async updateJob(jobId: string, state: JobState, note?: string) {
+    await this.initialized();
+
     if (!this.isJobState(state)) {
       return;
     }
@@ -73,14 +84,20 @@ export class PostGisConnection {
   }
 
   async dropJob(job: string) {
+    await this.initialized();
+
     return Jobs.delete({ job_id: job });
   }
 
   async dropFeaturesByColid(colId: string) {
+    await this.initialized();
+
     return Collections.delete({ coll_id: colId });
   }
 
   async pgCopyInsert(file: string) {
+    await this.initialized();
+
     try {
       const queryRunner = this.conn.createQueryRunner();
       const pgConn = await (<PostgresQueryRunner>queryRunner).connect();
@@ -93,12 +110,14 @@ export class PostGisConnection {
         )
       );
     } catch (err) {
-      throw new Error("error in copyinsert");
+      throw new Error("error in copyinsert:\n" + err);
     }
   }
 
   // TODO make this PATCH
   async pgCopyPatch(file: string) {
+    await this.initialized();
+
     const queryRunner = this.conn.createQueryRunner();
     const pgConn = await (<PostgresQueryRunner>queryRunner).connect();
     await pipeline(
@@ -113,6 +132,8 @@ export class PostGisConnection {
 
   // TODO ake this PUT
   async pgCopyPut(file: string) {
+    await this.initialized();
+
     const queryRunner = this.conn.createQueryRunner();
     const pgConn = await (<PostgresQueryRunner>queryRunner).connect();
     await pipeline(
@@ -126,16 +147,22 @@ export class PostGisConnection {
   }
 
   async listCollections() {
+    await this.initialized();
+
     const colls = Collections.find();
     return colls;
   }
 
   async getCollectionById(colId: string) {
+    await this.initialized();
+
     const coll = Collections.find({ where: { coll_id: colId } });
 
     return coll;
   }
   async getFeaturesByCollectionId(colId: string, limit?: number) {
+    await this.initialized();
+
     const feats = Features.createQueryBuilder()
       .where("ft_collection = :id", {
         id: colId,
@@ -147,6 +174,8 @@ export class PostGisConnection {
   }
 
   async getFeatureByCollectionIdAndFeatureId(colId: string, featId: string) {
+    await this.initialized();
+
     const feat = Features.createQueryBuilder()
       .where("ft_collection = :col_id", { col_id: colId })
       .andWhere("feature_id = :ft_id", { ft_id: featId })
@@ -157,11 +186,57 @@ export class PostGisConnection {
 
   // FOR TESTING ----------
   async countJobs() {
+    await this.initialized();
+
     // = this.conn.manager.find(Jobs);
     return Jobs.count();
   }
 
   async countFeatures() {
+    await this.initialized();
+
     return Features.count();
+  }
+
+  async close() {
+    await this.conn.destroy();
+  }
+
+  async mvtDummyData() {
+    await this.initialized();
+
+    const data = Features.query(
+      "WITH mvtgeom\
+       AS(SELECT ST_AsMVTGeom(geom, ST_TileEnvelope(0, 0, 0), extent => 4096, buffer => 64) AS geom, properties \
+      FROM features_test \
+      WHERE geom && ST_TileEnvelope(0, 0, 0, margin => (64.0 / 4096))) \
+      SELECT ST_AsMVT(mvtgeom.*, 'banana', 4096) FROM mvtgeom;"
+    );
+
+    return data;
+  }
+
+  async getMVT(
+    collId: string,
+    z: number,
+    x: number,
+    y: number,
+    extent = 4096,
+    buffer = 64,
+    name = "default"
+  ) {
+    // Prevent division by 0
+    if (extent < 1) throw new Error("Extent must be > 1");
+
+    const buffer_fp = buffer * 1.0;
+    const feature_table = "features_test";
+    const mvt_tmpl = `WITH mvtgeom AS ( \
+    SELECT ST_AsMVTGeom(geom, ST_TileEnvelope(${z},${x},${y}, extent => ${extent},  buffer => ${buffer}) AS geom, properties \
+      FROM ${feature_table} \
+      WHERE geom && ST_TileEnvelope(${z},${x},${y}, margin=> (${buffer_fp}/${extent}))) \
+    SELECT ST_AsMVT(mvtgeom.*, '${name}') FROM mvtgeom;`;
+
+    const mvt_resp = Features.query(mvt_tmpl);
+    return mvt_resp;
   }
 }
