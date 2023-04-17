a0744f155b298994f1519c69ac1561cb1447e689
Chris Brinker <christopherbrinker@web.de>
2023-04-17 14:01:32 +0200

    removed unused import

--- src/plugins/jobManager.ts
+++ src/plugins/jobManager.ts
@@ -1,16 +1,24 @@
-// Plugin that handles execution of jobs between the app and the database.
+/* Plugin that handles execution of jobs between the app and the database.
+Handles the creation of jobs, updating of job state, and execution of underlying tasks.
+
+! Can we have this implemented already?
+*/
 
 import { FastifyPluginAsync } from "fastify";
 import fp from "fastify-plugin";
 import { JobState } from "src/entities/jobs.js";
 import { Collections } from "src/entities/collections.js";
-import { PostGisConnection } from "src/util/PostGisConnection.js";
+//import { PostGisConnection } from "src/util/PostGisConnection.js";
 
 declare module "fastify" {
   interface FastifyInstance {
     jobManager: JobManager;
   }
 }
+
+// general Job type able to be executed by the jobManager
+// ? this required?
+type Job = () => Promise<any>;
 
 interface JobManager {
   createJob(): Promise<string>;
@@ -38,7 +46,10 @@ const jobManager: FastifyPluginAsync = async (fastify) => {
     ) {
       await fastify.db.updateJob(jobId, state, colId, note);
     },
-    async executeTaskAndUpdateJob(job: () => Promise<any>) {
+    async executeTaskAndUpdateJob(
+      job: () => Promise<any>,
+      callback: () => void
+    ) {
       const jobSuccess = await job();
     },
   });
