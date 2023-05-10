909dc87020c6d802f9451782e826cda706679fda
Chris Brinker <christopherbrinker@web.de>
2023-05-10 12:25:51 +0200

    frontend now includes more methods for visualisation, WIP

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,23 +1,32 @@
 import Map from "ol/Map";
 import View from "ol/View";
-import TileLayer from "ol/layer/WebGLTile";
-import OSM from "ol/source/OSM";
 import MVT from "ol/format/MVT";
-import DataTile from "ol/source/DataTile";
 import VectorTileLayer from "ol/layer/VectorTile";
+import TileLayer from "ol/layer/WebGLTile";
+import DataTile from "ol/source/DataTile";
+import OSM from "ol/source/OSM";
 import VectorTileSource from "ol/source/VectorTile";
 
-import VectorLayer from "ol/layer/Vector";
-import { Style, Stroke, Fill } from "ol/style";
-import { Interaction, Modify, Draw, Snap } from "ol/interaction";
-import { Vector as VectorSource } from "ol/source";
 import { Type } from "ol/geom/Geometry";
+import { Draw, Interaction, Modify, Snap } from "ol/interaction";
+import VectorLayer from "ol/layer/Vector";
 import { transform } from "ol/proj";
+import { Vector as VectorSource } from "ol/source";
+import { Fill, Stroke, Style } from "ol/style";
 
 import * as turf from "@turf/turf";
 
 import Feature from "ol/Feature";
-import { MultiPolygon, Polygon } from "ol/geom";
+import { LineString, MultiPolygon, Polygon } from "ol/geom";
+
+import {
+  philippineSea1,
+  philippineSea2,
+  setzensack1,
+  setzensack2,
+  titanicSeaRoutePlanned,
+  titanicSeaRouteActual,
+} from "./prefabGeo";
 
 const postgisCalcedDelta = turf.polygon([
   [
@@ -46,6 +55,8 @@ const postgisCalcedDelta = turf.polygon([
 
 const drawLayer = new VectorSource();
 const deltaLayer = new VectorSource();
+const prefabLayer1 = new VectorSource();
+const prefabLayer2 = new VectorSource();
 
 drawLayer.addFeature(
   new Feature({
@@ -53,49 +64,8 @@ drawLayer.addFeature(
   })
 );
 
-const poly1 = turf.polygon([
-  [
-    [1366163.7838552229, 6197542.917247698],
-    [1366325.018211762, 6197517.836347791],
-    [1366498.1958539705, 6197473.646190815],
-    [1366537.60869668, 6197440.204990939],
-    [1366554.3292966175, 6197504.698733555],
-    [1366703.620367487, 6197412.7354339],
-    [1366598.519453595, 6197373.32259119],
-    [1366603.2967678627, 6197296.885562905],
-    [1366452.8113684263, 6197287.330934369],
-    [1366444.4510684574, 6197226.420177454],
-    [1366174.5328123255, 6197252.6954059275],
-    [1366098.0957840404, 6197290.91392007],
-    [1366163.7838552229, 6197542.917247698],
-  ],
-]);
-
-const poly2 = turf.polygon([
-  [
-    [1366399.0665829133, 6197575.164119005],
-    [1366389.5119543776, 6197685.042347166],
-    [1366470.7262969306, 6197685.042347166],
-    [1366694.0657389513, 6197569.192476171],
-    [1366790.8063528747, 6197534.556947729],
-    [1366751.3935101652, 6197415.124091034],
-    [1366739.4502244957, 6197416.318419601],
-    [1366703.620367487, 6197412.7354339],
-    [1366598.519453595, 6197373.32259119],
-    [1366603.2967678627, 6197296.885562905],
-    [1366452.8113684263, 6197287.330934369],
-    [1366444.4510684574, 6197226.420177454],
-    [1366174.5328123255, 6197252.6954059275],
-    [1366098.0957840404, 6197290.91392007],
-    [1366163.7838552229, 6197542.917247698],
-    [1366325.018211762, 6197517.836347791],
-    [1366395.4835972125, 6197498.72709072],
-    [1366399.0665829133, 6197575.164119005],
-  ],
-]);
-
-const intersect = turf.intersect(poly1, poly2);
-const union = turf.union(poly1, poly2);
+const intersect = turf.intersect(setzensack1, setzensack2);
+const union = turf.union(setzensack1, setzensack2);
 
 let delta;
 if (union && intersect) {
@@ -121,11 +91,11 @@ const drawStyle = function () {
   return [
     new Style({
       stroke: new Stroke({
-        color: "rgba(0, 255, 0, 1.0)",
+        color: "rgba(40, 128, 200, 1.0)",
         width: 2,
       }),
       fill: new Fill({
-        color: "rgba(0, 255, 0, 0.2)",
+        color: "rgba(40, 128, 200, 0.3)",
       }),
     }),
   ];
@@ -193,24 +163,16 @@ const map = new Map({
     new TileLayer({
       source: new OSM(),
     }),
-
     new VectorTileLayer({
-      //declutter: true,
+      declutter: true,
       source: new VectorTileSource({
         format: new MVT(),
-        tileLoadFunction: function (tile, url) {
-          console.log("tileLoadFunction", url);
-          //console.log(VectorTileSource.prototype.tileLoadFunction);
-          //VectorTileSource.prototype.tileLoadFunction(tile, url);
-        },
-
         url: `/collections/${coll1}/{z}/{x}/{y}.vector.pbf`,
       }),
-
       style: styleGreen(),
     }),
     new VectorTileLayer({
-      //declutter: true,
+      declutter: true,
       source: new VectorTileSource({
         format: new MVT(),
         url: `/collections/${coll2}/{z}/{x}/{y}.vector.pbf`,
@@ -224,6 +186,14 @@ const map = new Map({
     new VectorLayer({
       source: drawLayer,
       style: drawStyle(),
+    }),
+    new VectorLayer({
+      source: prefabLayer1,
+      style: styleRed(),
+    }),
+    new VectorLayer({
+      source: prefabLayer2,
+      style: styleGreen(),
     }),
     // Vector Tile position overlay
     new TileLayer({
@@ -251,12 +221,13 @@ const map = new Map({
 
   view: new View({
     //projection: "EPSG:4326",
-    center: [0, 0],
-    zoom: 2,
+    center: [14483488.424272656, 580557.970539473],
+    zoom: 4,
   }),
 });
 
 // Create listeners for jumping to a specific location
+// TODO fix mercator
 const tileCoordZ = document.getElementById("tileCoordZ");
 const tileCoordX = document.getElementById("tileCoordX");
 const tileCoordY = document.getElementById("tileCoordY");
@@ -282,7 +253,6 @@ document.getElementById("deltaButton")?.addEventListener("click", function () {
 
   const ft1 = feature1 as Feature;
   const ft2 = feature2 as Feature;
-
   console.log("Ft1:", ft1);
   console.log("Ft2:", ft2);
 });
@@ -340,24 +310,82 @@ document
     }
   });
 
+document
+  .getElementById("prefabSelectButton")
+  ?.addEventListener("click", function () {
+    console.log("prefabSelectButton");
+    const selectedPrefab = document.getElementById("prefabProvider")!;
+    const selectedPrefabValue = (<HTMLInputElement>selectedPrefab).value;
+
+    if (selectedPrefabValue) {
+      switch (selectedPrefabValue) {
+        case "titanic":
+          prefabLayer1.addFeature(
+            new Feature({
+              geometry: new LineString(
+                titanicSeaRoutePlanned.geometry.coordinates
+              ),
+            })
+          );
+          prefabLayer2.addFeature(
+            new Feature({
+              geometry: new LineString(
+                titanicSeaRouteActual.geometry.coordinates
+              ),
+            })
+          );
+          break;
+        case "philippines":
+          prefabLayer1.addFeature(
+            new Feature({
+              geometry: new Polygon(philippineSea1.geometry.coordinates),
+            })
+          );
+          prefabLayer2.addFeature(
+            new Feature({
+              geometry: new Polygon(philippineSea2.geometry.coordinates),
+            })
+          );
+          break;
+        case "midway":
+          throw new Error("Not implemented");
+      }
+    }
+  });
+
+document
+  .getElementById("prefabClearButton")
+  ?.addEventListener("click", function () {
+    prefabLayer1.clear();
+    prefabLayer2.clear();
+  });
+
 // Create interation for drawing a polygon
 drawLayer.on("addfeature", function (e) {
   console.log("drawlayer");
-
   if (e.feature) {
-    console.log(e.feature);
-    console.log(e.feature.getGeometry()?.getSimplifiedGeometry(0.1));
+    console.log(
+      e.feature
+        .getGeometry()
+        ?.getSimplifiedGeometry(0.1)
+        //@ts-expect-error - getCoordinates() is not in the type definition
+        .getCoordinates()
+        .toString()
+    );
   }
+});
+drawLayer.on("changefeature", function (e) {
+  console.log("changefeature");
+  if (e.feature)
+    console.log(e.feature.getGeometry()?.getSimplifiedGeometry(0.1));
 });
 
 const modify = new Modify({
   source: drawLayer,
 });
 map.addInteraction(modify);
-
 let draw: Interaction, snap: Interaction; // global so we can remove them later
 const typeSelect = document.getElementById("type")!;
-
 function addInteractions() {
   draw = new Draw({
     source: drawLayer,
@@ -379,6 +407,8 @@ typeSelect.onchange = function () {
 };
 
 addInteractions();
-
+/*map.getLayers()[1].on("tileloadend", function (e) {
+  console.log("tileloadend");
+});*/
 //@ts-expect-error - just for debugging
 window.map = map;
